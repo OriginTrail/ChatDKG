a5d44ae1c936b22ba2399ce04711949ad41b8040
Branimir Rakic <33914812+branarakic@users.noreply.github.com>
2023-07-21 17:44:34 +0200

    Update dkg-demo.js

--- examples/langchain/dkg-demo.js
+++ examples/langchain/dkg-demo.js
@@ -1,9 +1,14 @@
+// This is a simple example of creating a Knowledge Asset from a JSON file on OriginTrail DKG
+
 import "dotenv/config";
 import DKG from "dkg.js";
 import fs from "fs";
 
+// Load the content of the Knowledge Asset (using an imaginary medicine "Yewmakerol" leaflet as content
 const yewmakerol = JSON.parse(fs.readFileSync("./yewmakerol.json"));
 
+
+// initialize the DKG client on OriginTrail DKG Testnet
 const dkg = new DKG({
   endpoint: process.env.OT_NODE_HOSTNAME,
   blockchain: {
@@ -14,6 +19,7 @@ const dkg = new DKG({
 });
 
 (async () => {
+   // creating a Knowledge Asset on OriginTrail DKG
     const creationResult = await dkg.asset.create(yewmakerol, { epochsNum: 5 });
 
     console.log(`Knowledge asset UAL: ${creationResult.UAL}`);
